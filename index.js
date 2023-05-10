8dfb29ce2f069cff95c7f28e0f69701be44f7500
Nadya Mincheva <7000585m1@gmail.com>
2023-05-10 12:41:40 +0300

    new button

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const myString = [
-    "Поцеловать Локи в носик",
-    "Подарить Сильвии хахаски",
-    "Покушать",
-    "Побешать",
-    "Сказать Билли, что он котик",
-    "Укусить Люка за нос",
-    "Зацеловать Волче"
-];
+const myString = [];
 
 let currentIndex = myString.length;
 
@@ -15,24 +7,42 @@ function shuffleArray(array) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
-}
+};
 
 function getNextText() {
+  if (myString.length === 0) {
+    return "add your text";
+  }
   if (currentIndex === myString.length) {
     shuffleArray(myString);
     currentIndex = 0;
   }
-
   const text = myString[currentIndex];
   currentIndex++;
-
   return text;
-}
+};
 
 const generateButton = document.getElementById('generate');
 const textElement = document.getElementById('text');
+const addPhraseButton = document.getElementById('add-phrase-button');
+const newPhraseInput = document.getElementById('new-phrase-input');
+const deleteAllButton = document.getElementById('delete-all-button');
 
 generateButton.addEventListener('click', function() {
   textElement.innerHTML = getNextText();
 });
 
+addPhraseButton.addEventListener('click', function() {
+  const newPhrase = newPhraseInput.value;
+  if (newPhrase !== "") {
+    myString.push(newPhrase);
+    currentIndex = myString.length;
+    newPhraseInput.value = "";
+  }
+});
+
+deleteAllButton.addEventListener('click', function() {
+  myString.length = 0;
+  currentIndex = myString.length;
+  textElement.innerHTML = "";
+});
